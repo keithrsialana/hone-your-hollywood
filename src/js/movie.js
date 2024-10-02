85127bb2d460e641790de56e115433444b0eddf8
RiverStephenson <riverstephenson06@outlook.com>
2024-10-02 11:17:33 -0500

    started to add time interval for countdown in movie.js

--- src/js/movie.js
+++ src/js/movie.js
@@ -1,10 +1,33 @@
-const backButton = document.querySelector('#btnBack')
-let redirectURL = './index.html'
+const backButton = document.querySelector('#btnBack');
+let redirectURL = './index.html';
 
-const redirect = function(url) {
+const timeELement = document.querySelector('.movie');
+const timeLeft = 3;
+
+
+const redirect = function (url) {
     redirectURL = url;
     location.assign(url);
 }
-backButton.addEventListener('click', function(){
+backButton.addEventListener('click', function () {
     redirect(redirectURL);
-})
+});
+
+function setTime() {
+    const timerInterval = setInterval(function () {
+        timeLeft--;
+        timeELement.textContent = timeLeft;
+        if (timeLeft === 0) {
+            clearInterval(timerInterval);
+            sendMessage();
+        }
+    }, 1000);
+}
+function sendMessage() {
+    timeELement.textContent = '';
+    let imgElement = document.createElement('img');
+    imgElement.setAttribute('src', 'https://images.pexels.com/photos/3945319/pexels-photo-3945319.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+    timeELement.appendChild(imgElement);
+};
+
+setTime();
