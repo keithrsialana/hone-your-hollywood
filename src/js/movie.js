19cb501e904376ec3aa8ae55b2823165fcd5be80
unknown <Jsilber60@gmail.com>
2024-10-02 11:57:44 -0400

    added the modal on the back button that redirects us to the home page

--- src/js/movie.js
+++ src/js/movie.js
@@ -1,10 +1,17 @@
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+
+
+
 const backButton = document.querySelector('#btnBack')
 let redirectURL = './index.html'
 
 const redirect = function(url) {
     redirectURL = url;
     location.assign(url);
-}
+   
+}   
 backButton.addEventListener('click', function(){
     redirect(redirectURL);
 })
