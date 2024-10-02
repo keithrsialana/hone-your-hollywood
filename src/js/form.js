dda06b2e02e695de15a7e7f8713491f93808ef3c
Keith Sialana <ninjabasketball@hotmail.com>
2024-10-02 12:10:25 -0400

    did stuff

--- src/js/form.js
+++ src/js/form.js
@@ -1,3 +1,16 @@
-let ls = readLocalStorage();
 let pickButton = document.getElementById('btnPickMovie');
 let taMovies = document.getElementById('savedMovies');
+let genreList = document.getElementById('genreList');
+let genres = document.querySelectorAll('li');
+
+genres.forEach(element => {
+    element.addEventListener("click", function(){
+        // removes selected class from any other list item that has it
+        genres.forEach(item => {
+            if (item.classList.contains('selected'))
+                item.classList.remove('selected');
+        });
+        // adds selected class to the clicked list item
+        element.classList.add('selected');
+    });
+});
