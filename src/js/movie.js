b89dc5ee461486cd443ec6ddaddc708e7b0fecaa
RiverStephenson <riverstephenson06@outlook.com>
2024-10-02 11:23:07 -0500

    changed movie.js

--- src/js/movie.js
+++ src/js/movie.js
@@ -1,10 +1,13 @@
-const backButton = document.querySelector('#btnBack')
-let redirectURL = './index.html'
+const myModal = document.getElementById('myModal');
+const myInput = document.getElementById('myInput');
+const backButton = document.querySelector('#btnBack');
+let redirectURL = './index.html';
 
-const redirect = function (url) {
+const redirect = function(url) {
     redirectURL = url;
     location.assign(url);
-}
+   
+}   
 backButton.addEventListener('click', function(){
     redirect(redirectURL);
-})
+});
